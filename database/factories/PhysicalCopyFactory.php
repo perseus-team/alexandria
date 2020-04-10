0e5bc888da14c1832d7b279e0f99c7e2886e6ef8
Toby Selway <tobyselway@outlook.com>
2020-04-11 00:04:41 +0100

    Implemented PhysicalCopy factory

--- database/factories/PhysicalCopyFactory.php
+++ database/factories/PhysicalCopyFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(PhysicalCopy::class, function (Faker $faker) {
     return [
-        //
+        'book_id'=>factory(\App\Book::class)->create()->id,
+        "location" => $faker->randomElement([null, $faker->city . ", " . $faker->country]),
+        "notes" => $faker->realText(200, 2),
+        'user_id'=>factory(\App\User::class)->create()->id
     ];
 });
